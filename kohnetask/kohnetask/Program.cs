147719f6b0ff3a350bb48ae5d3058a191b3c6e5a
metinvahid <matinav@code.edu.az>
2023-12-01 10:36:39 -0800

    metinincommiti

---
+++ kohnetask/kohnetask/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace tasklarvsc2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Matin";
+            string surname = "Vahid";
+            Console.WriteLine(name + " " + surname);
+            float num1 = 4;
+            float num2 = 6;
+            float num3 = num1 + num2;
+            Console.WriteLine(num3);
+            float num4 = 2;
+            float num5 = 7;
+            float num6 = num4 - num5;
+            Console.WriteLine(num6);
+            float num7 = num1 * num2;
+            float num8 = num7 * num7;s
+            Console.WriteLine(num8);
+            float num9 = num5 / num4;
+            Console.WriteLine(num9);
+            float num10 = num9 * num9;
+            Console.WriteLine(num10);
+
+
+            Console.ReadKey();
+        }
+    }
+}
